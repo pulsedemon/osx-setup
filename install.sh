23ea000a22096492162e42947d8dc02c4c8a6aae
Michael Squashic <msquashic@gmail.com>
2017-05-03 15:26:15 -0400

    added missing rvm install command

--- install.sh
+++ install.sh
@@ -24,7 +24,11 @@ brew cask install google-chrome
 brew cask install spotify
 brew cask install spectacle
 
+# install rvm
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+
+# install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 
 # install atom packages
